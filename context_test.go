9de58d9cb7bcfd081e7c9f73d9f05d426307878d
DevanshuTripathi <aditheprince120@gmail.com>
2026-05-17 01:04:23 +0530

    feat: added tests for key value store in context

--- context_test.go
+++ context_test.go
@@ -1,6 +1,10 @@
 package vodka
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestAbortStopsChain(t *testing.T) {
 	calls := []string{}
@@ -22,4 +26,51 @@ func TestAbortStopsChain(t *testing.T) {
 	if len(calls) != 1 || calls[0] != "h1" {
 		t.Errorf("got %v, want [h1]", calls)
 	}
+}
+
+func TestKeys(t *testing.T) {
+	app := DefaultRouter()
+
+	app.Use(func(c *Context) {
+		c.Set("M1", "First")
+
+		c.Next()
+	})
+
+	app.Use(func(c *Context) {
+		c.Set("M2", "Second")
+
+		c.Next()
+	})
+
+	app.Use(func(c *Context) {
+		c.Set("M1", "Changed")
+
+		c.Next()
+	})
+
+	app.GET("/test", func(c *Context) {
+		m1, exists := c.Get("M1")
+		if !exists {
+			t.Fatal("Value for M1 does not exist")
+		}
+
+		if m1.(string) != "Changed" {
+			t.Fatalf("expected Changed, got %v", m1)
+		}
+
+		m2, exists := c.Get("M2")
+		if !exists {
+			t.Fatal("Value for M2 does not exist")
+		}
+
+		if m2.(string) != "Second" {
+			t.Fatalf("expected Second, got %v", m2)
+		}
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/test", nil)
+	w := httptest.NewRecorder()
+
+	app.ServeHTTP(w, req)
 }
